e8d14267c1f9b6c597a8cdff9c0e252c034f44c8
Valentino Wong <valentino.wong@gmail.com>
2019-07-19 11:28:46 -0400

    finished mock code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,27 @@ def reload
   load 'config/environment.rb'
 end
 
-
 # Put your variables here~!
+# valentino = Passenger.new("Valentino")
+# cynthia = Passenger.new("Cynthia")
+# chad = Passenger.new("Chad")
+# john = Passenger.new("John")
+# betty = Passenger.new("Betty")
+
+# dumbledore = Driver.new("Dumbledore")
+# mcgonagall = Driver.new("Mcgonagall")
+# snape = Driver.new("Snape")
+
+# ride1 = Ride.new(dumbledore, valentino, 10)
+# ride2 = Ride.new(snape, valentino, 50)
+# ride3 = Ride.new(mcgonagall, john, 15)
+# ride4 = Ride.new(snape, chad, 25)
+# ride5 = Ride.new(dumbledore, cynthia, 30)
+# ride6 = Ride.new(mcgonagall, cynthia, 12)
+# ride7 = Ride.new(snape, cynthia, 5)
+# ride8 = Ride.new(mcgonagall,betty, 40)
+# ride9 = Ride.new(snape,john, 2)
+# ride10 = Ride.new(snape,betty, 100)
+# ride11 = Ride.new(mcgonagall,betty, 22)
 
 binding.pry
